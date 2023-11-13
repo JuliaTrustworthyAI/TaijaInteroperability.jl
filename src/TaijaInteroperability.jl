24404a50bc83284d1d8cb56a07e4c7a935aff171
kmariuszk <kiciorm@gmail.com>
2023-11-13 13:51:00 +0100

    Add backward compatibility

--- src/TaijaInteroperability.jl
+++ src/TaijaInteroperability.jl
@@ -1,5 +1,10 @@
 module TaijaInteroperability
 
+using PackageExtensionCompat
+function __init__()
+    @require_extensions
+end
+
 include("CounterfactualExplanations/CounterfactualExplanations.jl")
 
 end
